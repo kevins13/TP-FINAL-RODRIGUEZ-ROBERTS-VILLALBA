53d3da08f6baa535fb574ef7ad12ac02ce2a46d3
kevin villalba <51637211+kevins13@users.noreply.github.com>
2019-11-10 12:32:03 -0300

    segunda entrega
    segunda entrega

---
+++ segunda entrega/Cliente/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Cliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Enviar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Enviar
+            // 
+            this.Enviar.Location = new System.Drawing.Point(279, 239);
+            this.Enviar.Name = "Enviar";
+            this.Enviar.Size = new System.Drawing.Size(75, 23);
+            this.Enviar.TabIndex = 0;
+            this.Enviar.Text = "Enviar";
+            this.Enviar.UseVisualStyleBackColor = true;
+            this.Enviar.Visible = false;
+            this.Enviar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(27, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(327, 212);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Usuario";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(109, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Conectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(389, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(158, 90);
+            this.treeView1.TabIndex = 6;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(373, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "arrastre Archivo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Location = new System.Drawing.Point(376, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(66, 100);
+            this.panel1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(449, 108);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 284);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Enviar);
+            this.Name = "Form1";
+            this.Text = "krausecord";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Enviar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ segunda entrega/Cliente/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Cliente
+{    
+
+    public partial class Form1 : Form
+    {
+
+        static private NetworkStream stream;//permitemanejar el flujo de informacion meanejarlo
+        static private StreamWriter streamw;//permite escribir informacion en el flojo
+        static private StreamReader streamr;//permite de el flujo de informacion
+        static private TcpClient client = new TcpClient();//nos permite conectarnos al flijo de informacion
+        static private string nick = "unknown";
+
+        private delegate void DAddItem(String s); //sirve para mandar informacion entre procesos
+        //*investigar delegados*//
+            
+              
+        
+        private void AddItem(String s)
+        {
+            listBox1.Items.Add(s);// se agrega el texto que esta en el flujo de informacion
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            streamw.WriteLine(textBox1.Text);
+            streamw.Flush();
+            textBox1.Clear();
+
+
+        }
+
+
+         void Listen()
+        {
+            while (client.Connected)//cuando el cliente este conectado
+            {
+                try
+                {
+                    this.Invoke(new DAddItem(AddItem), streamr.ReadLine());//se invoca al delegado que permite intecambiar informacion entre procesos deinido en las variables
+                   // de no hacer esto no se puede hacer la comunicacion directa porque estamos en procesos diferentes
+                }
+                catch
+                {
+                    MessageBox.Show("No se ha podido conectar al servidor");
+                    Application.Exit();
+                }
+            }
+        }
+
+         void Conectar()//aca se conecta al servidor
+        {
+            try
+            {
+                client.Connect("192.168.0.15", 8000);// poner el el cmd ipconfig y copiar lo que hay en el ipv4
+                if (client.Connected)
+                {
+                    Thread t = new Thread(Listen);// si el cliente se conecta al hilo(procesos que trabajan a la vez), acepta por parametro la funcion listen
+
+                    stream = client.GetStream();// se obtiene el flujo de informacion
+                    streamw = new StreamWriter(stream);//busca que se quiere escribir
+                    streamr = new StreamReader(stream);//leer de nuestro flijo de informacion
+
+                    streamw.WriteLine(nick);//se manda al flujo de informacion el nick
+                    streamw.Flush();//se limpia el flujo despues de enviar el mensaje
+
+                    t.Start();//comienza el proceso
+                }
+                else
+                {
+                    MessageBox.Show("Servidor no Disponible");
+                    Application.Exit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Servidor no Disponible");
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            textBox2.Visible = false;
+            button2.Visible = false;
+            listBox1.Visible = true;
+            textBox1.Visible = true;
+            Enviar.Visible = true;
+
+            nick = textBox2.Text;
+
+            Conectar();
+
+        }
+        private TreeNode CreaarArbol(DirectoryInfo  directoryInfo)
+        {
+            TreeNode treeNode = new TreeNode(directoryInfo.Name);
+
+            foreach(var item in directoryInfo.GetDirectories())
+            {
+                treeNode.Nodes.Add(CreaarArbol(item));
+            }
+
+            foreach(var item in directoryInfo.GetFiles())
+            {
+                treeNode.Nodes.Add(new TreeNode(item.Name));
+            }
+            return treeNode;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string rutaBase = "C:\\Users\\necho\\Desktop\\cosas";
+            treeView1.Nodes.Clear();
+            DirectoryInfo directoryInfo = new DirectoryInfo(rutaBase);
+
+            treeView1.Nodes.Add(CreaarArbol(directoryInfo));
+
+            panel1.AllowDrop = true;
+            panel1.DragEnter += new DragEventHandler(methodDragEnter);
+            panel1.DragDrop += new DragEventHandler(methodDragDrop);
+        }
+        public void methodDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+        public void methodDragDrop(object sender, DragEventArgs e)
+        {
+            listBox2.Items.Clear();
+
+            string[] archivos = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach(string item in archivos)
+            {
+                listBox2.Items.Add(item);
+            }
+
+        }
+        private void treeView1_DoubleClick(object sender, EventArgs e)
+        {
+            string rutaAbsoluta = "C:\\Users\\necho\\Desktop\\" + treeView1.SelectedNode.FullPath;
+
+            System.Diagnostics.Process.Start(rutaAbsoluta);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
